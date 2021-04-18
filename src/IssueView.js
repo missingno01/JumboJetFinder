3d780b66057eb90749992bdae49162356f4e86d8
missingno01 <ringofwallfes@gmail.com>
2021-04-18 00:03:30 -0700

    Carbon Emissions are working

--- src/IssueView.js
+++ src/IssueView.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+
+    let emissions = new Map([
+        ['74H', '2.82 L/100 km'],
+        ['744', '3.34 L/100 km'],
+        ['343', '3.49 L/100 km'],
+        ['359', '2.39 L/100 km'],
+        ['333', '2.98 L/100 km'],
+        ['100', '9.99 L/100 km'],
+        ['141', '9.99 L/100 km'],
+        ['142', '9.99 L/100 km'],
+        ['143', '9.99 L/100 km'],
+        ['312', '9.99 L/100 km'],
+        ['313', '9.99 L/100 km'],
+        ['318', '9.99 L/100 km'],
+        ['319', '2.95 L/100 km'],
+        ['31A', '9.99 L/100 km'],
+        ['31B', '2.89 L/100 km'],
+        ['31N', '1.93 L/100 km'],
+        ['320', '2.61 L/100 km'],
+        ['321', '2.50 L/100 km'],
+        ['32A', '2.56 L/100 km'],
+        ['32B', '2.45 L/100 km'],
+        ['77W', '2.91 L/100 km'],
+    ])
+
+
 class IssueView extends React.Component {
 
 
@@ -20,6 +46,10 @@ class IssueView extends React.Component {
         <div className="field content">
           <span className="label">Destination: </span>
           <span className="value">{issue.legs[0].destination}</span>
+        </div>
+        <div className="field">
+          <span className="label2">Carbon Emissions:&nbsp; </span>
+          <span className="value">{emissions.get(issue.legs[0].aircraftType)}</span>
         </div>
         <Link to="/IssueDetails" >Get Details</Link>
       </div>
