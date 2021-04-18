e1da77277d20c67c0db116495985d6e9972e5c09
missingno01 <ringofwallfes@gmail.com>
2021-04-17 23:56:51 -0700

    More aircraft?

--- src/IssueList.js
+++ src/IssueList.js
@@ -85,6 +85,31 @@ class IssueList extends Component {
   }
 
   render() {
+
+    let emissions = new Map([
+        ['74H', '2.82 L/100 km'],
+        ['744', '3.34 L/100 km'],
+        ['343', '3.49 L/100 km'],
+        ['359', '2.39 L/100 km'],
+        ['333', '2.98 L/100 km'],
+        ['100', '9.99 L/100 km'],
+        ['141', '9.99 L/100 km'],
+        ['142', '9.99 L/100 km'],
+        ['143', '9.99 L/100 km'],
+        ['312', '9.99 L/100 km'],
+        ['313', '9.99 L/100 km'],
+        ['318', '9.99 L/100 km'],
+        ['319', '2.95 L/100 km'],
+        ['31A', '9.99 L/100 km'],
+        ['31B', '2.89 L/100 km'],
+        ['31N', '1.93 L/100 km'],
+        ['320', '2.61 L/100 km'],
+        ['321', '2.50 L/100 km'],
+        ['32A', '2.56 L/100 km'],
+        ['32B', '2.45 L/100 km'],
+        ['77W', '2.91 L/100 km'],
+    ])
+
     const { classes } = this.props;
     let issueViews;
     if (this.state.isLoaded) {
@@ -126,6 +151,24 @@ class IssueList extends Component {
               <MenuItem value={'744'}>Boeing 747-400</MenuItem>
               <MenuItem value={'74H'}>Boeing 747-800</MenuItem>
               <MenuItem value={'343'}>Airbus A340-300</MenuItem>
+              <MenuItem value={'359'}>Airbus A350-900</MenuItem>
+              <MenuItem value={'333'}>Airbus A330-300</MenuItem>
+              <MenuItem value={'100'}>Fokker 100</MenuItem>
+              <MenuItem value={'141'}>BAE Systems 146-100</MenuItem>
+              <MenuItem value={'142'}>BAE Systems 146-200</MenuItem>
+              <MenuItem value={'143'}>BAE Systems 146-300</MenuItem>
+              <MenuItem value={'312'}>Airbus A310-200</MenuItem>
+              <MenuItem value={'313'}>Airbus A310-300</MenuItem>
+              <MenuItem value={'318'}>Airbus A318</MenuItem>
+              <MenuItem value={'319'}>Airbus A319</MenuItem>
+              <MenuItem value={'31A'}>Airbus A318 (sharklets)</MenuItem>
+              <MenuItem value={'31B'}>Airbus A319 (sharklets)</MenuItem>
+              <MenuItem value={'31N'}>Airbus A139neo</MenuItem>
+              <MenuItem value={'320'}>Airbus A320</MenuItem>
+              <MenuItem value={'321'}>Airbus A321</MenuItem>
+              <MenuItem value={'32A'}>Airbus A320 (sharklets)</MenuItem>
+              <MenuItem value={'32B'}>Airbus A321 (sharklets)</MenuItem>
+              <MenuItem value={'77W'}>Boeing 777-300ER</MenuItem>
             </Select>
             <FormHelperText>Select an aircraft type</FormHelperText>
           </FormControl>
