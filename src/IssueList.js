16a6959d8ea97ff2de504bb3d9924f601a220ce0
Farhan Saeed <fasaeed@ucsc.edu>
2021-04-17 19:20:49 -0700

    just need to add params to the request from state and we should be almost done

--- src/IssueList.js
+++ src/IssueList.js
@@ -5,8 +5,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-// import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import { withStyles } from "@material-ui/core/styles";
+import axios from 'axios';
 
 const styles = (theme) => ({
   formControl: {
@@ -27,7 +28,8 @@ class IssueList extends Component {
       pageNum: 1,
       issues: [],
       airlines: "",
-      acType: ""
+      acType: "",
+      status: 0,
     };
     this.leftClick = this.leftClick.bind(this)
     this.rightClick = this.rightClick.bind(this)
@@ -35,37 +37,6 @@ class IssueList extends Component {
     this.handleAircraft = this.handleAircraft.bind(this)
   }
 
-
-
-  componentDidMount() {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer g2g7vkdmwzx7dt6stvvrt35s");
-    myHeaders.append("accept", "application/json");
-
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-    //https://api.github.com/repos/walmartlabs/thorax/issues
-    fetch("https://api.lufthansa.com/v1/flight-schedules/flightschedules/passenger?airlines=LH&startDate=01JUN21&endDate=09JUN21&daysOfOperation=1234567&timeMode=UTC", requestOptions)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-          this.setState({
-            isLoaded: true,
-            issues: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
   leftClick() {
 
     // Changing state 
@@ -81,75 +52,104 @@ class IssueList extends Component {
     }
   }
   handleAirlines(e) {
-    this.setState({airlines: e.target.value});
+    this.setState({ airlines: e.target.value });
   }
 
   handleAircraft(e) {
-    this.setState({acType: e.target.value});
+    this.setState({ acType: e.target.value });
     console.log(e);
   }
 
-  render() {
-    const {classes} = this.props;
-    const issueViews = this.state.issues.slice((this.state.pageNum - 1) * 10, this.state.pageNum * 10).map(
-      issue => <IssueView key={issue.id} issue={issue} setDetails={this.props.setDetails} />
-    )
-    const { error, isLoaded, issues } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div className="container">
-          <h1>Jumbo Jet Finder| List of Flights</h1>
-          <form>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="demo-simple-select-helper-label">Airlines</InputLabel>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                value={this.state.airlines}
-                onChange={this.handleAirlines}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={'LH'}>Lufthansa</MenuItem>
-                <MenuItem value={'LX'}>Swiss Air</MenuItem>
-              </Select>
-              <FormHelperText>Select an airline</FormHelperText>
-            </FormControl>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="demo-simple-select-helper-label">Aircraft type</InputLabel>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                value={this.state.acType}
-                onChange={this.handleAircraft}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={'744'}>Boeing 747-400</MenuItem>
-                <MenuItem value={'74H'}>Boeing 747-800</MenuItem>
-                <MenuItem value={'343'}>Airbus A340-300</MenuItem>
-              </Select>
-              <FormHelperText>Select an aircraft type</FormHelperText>
-            </FormControl>
-          </form>
-          <h3>Page: {this.state.pageNum}</h3>
-          {(this.state.pageNum > 1) ? <button onClick={this.leftClick}>
-            Left
-          </button> : null}
-          {(this.state.pageNum < this.state.issues.length / 10) ? <button onClick={this.rightClick}>
-            Right
-          </button> : null}
+  handleSubmit(e) {
+    e.preventDefault();
+    var config = {
+      headers: {
+        'Authorization': 'Bearer g2g7vkdmwzx7dt6stvvrt35s'
+      }
+    };
 
-          {issueViews}
-        </div>
-      );
+    axios.get('https://api.lufthansa.com/v1/flight-schedules/flightschedules/passenger?airlines=LH&startDate=01JUN21&endDate=09JUN21&daysOfOperation=1234567&timeMode=UTC', config).then((res) => {
+      console.log(res)
+      if (res.status === 200 || res.status === 206) {
+        console.log(res.data);
+        this.setState({isLoaded: true, issues: res.data, status: res.status})
+      }
+    }).catch((err) => {
+      console.log(err)
+      this.setState({ status: err.response.status });
+    })
+  }
+
+  render() {
+    const { classes } = this.props;
+    let issueViews;
+    if (this.state.isLoaded) {
+      issueViews = this.state.issues.slice((this.state.pageNum - 1) * 10, this.state.pageNum * 10).map(
+        issue => <IssueView key={issue.id} issue={issue} setDetails={this.props.setDetails} />
+      )
     }
+    return (
+      <div className="container">
+        <h1>Jumbo Jet Finder| List of Flights</h1>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-helper-label">Airlines</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={this.state.airlines}
+              onChange={this.handleAirlines}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={'LH'}>Lufthansa</MenuItem>
+              <MenuItem value={'LX'}>Swiss Air</MenuItem>
+            </Select>
+            <FormHelperText>Select an airline</FormHelperText>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-helper-label">Aircraft type</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={this.state.acType}
+              onChange={this.handleAircraft}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={'744'}>Boeing 747-400</MenuItem>
+              <MenuItem value={'74H'}>Boeing 747-800</MenuItem>
+              <MenuItem value={'343'}>Airbus A340-300</MenuItem>
+            </Select>
+            <FormHelperText>Select an aircraft type</FormHelperText>
+          </FormControl>
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+        {this.state.isLoaded ?
+          <div>
+            <h3>Page: {this.state.pageNum}</h3>
+            {(this.state.pageNum > 1) ? <button onClick={this.leftClick}>
+              Left
+          </button> : null}
+            {(this.state.pageNum < this.state.issues.length / 10) ? <button onClick={this.rightClick}>
+              Right
+          </button> : null}
+          </div>
+          : null
+        }
+
+
+        {this.state.isLoaded ? issueViews : null}
+        {this.state.status === 400 ? <h1>Invalid parameters found, try again</h1> : null}
+        {this.state.status === 404 ? <h1>Couldn't find a flight with select parameters, try again</h1> : null}
+        {this.state.status === 500 ? <h1>Looks like the service is being overloaded, try again in a minute</h1> : null}
+      </div>
+    );
+
   }
 }
 
